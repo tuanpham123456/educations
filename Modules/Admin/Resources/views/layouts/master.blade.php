1df7902a4e64bf6b08beb837c98622184cf01a9c
tuanpham123456 <bicudu0403@gmail.com>
2020-10-29 20:04:07 +0700

    config (setting_admin.php)

--- Modules/Admin/Resources/views/layouts/master.blade.php
+++ Modules/Admin/Resources/views/layouts/master.blade.php
@@ -8,6 +8,7 @@
     <meta name="Description" content="Bootstrap Responsive Admin Web Dashboard HTML5 Template">
     <meta name="Author" content="Spruko Technologies Private Limited">
     <meta name="Keywords" content="admin,admin dashboard,admin dashboard template,admin panel template,admin template,admin theme,bootstrap 4 admin template,bootstrap 4 dashboard,bootstrap admin,bootstrap admin dashboard,bootstrap admin panel,bootstrap admin template,bootstrap admin theme,bootstrap dashboard,bootstrap form template,bootstrap panel,bootstrap ui kit,dashboard bootstrap 4,dashboard design,dashboard html,dashboard template,dashboard ui kit,envato templates,flat ui,html,html and css templates,html dashboard template,html5,jquery html,premium,premium quality,sidebar bootstrap 4,template admin bootstrap 4"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Title -->
     <title> Valex -  Premium dashboard ui bootstrap rwd admin html5 template </title>
     <!-- Favicon -->
---
+++ config/setting_admin.php
@@ -0,0 +1,22 @@
+<?php
+return[
+    'siderbar' => [
+      [
+          'name'  => 'Tổng quan',
+          'route' => ''
+      ],
+      [
+          'name'  => 'Khóa học',
+          'sub'   => [
+              [
+                  'name'  => 'Danh mục',
+                  'route' => ''
+              ],
+              [
+                  'name'  => 'Từ khóa',
+
+              ]
+          ]
+      ]
+    ]
+];
